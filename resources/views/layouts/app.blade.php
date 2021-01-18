af59f55a23c94f2ce7815307b737480770e1a217
huanggz <918031078@qq.com>
2021-01-18 16:39:36 +0800

    基础页面结构

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>@yield('title', 'LaraBBS') - LaraBBS论坛</title>
+
+  <!-- Style -->
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+</head>
+<body>
+  <div id="app" class="{{ route_class() }}-page">
+    @include('layouts._header')
+
+    <div class="container">
+      @include('shared._messages')
+      @yield('content')
+    </div>
+
+    @include('layouts._footer')
+  </div>
+
+  <!-- Script -->
+  <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
