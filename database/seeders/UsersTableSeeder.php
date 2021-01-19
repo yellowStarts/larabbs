ec561bbac429edc15f16999c10bacbb8b1a26ab1
huanggz <918031078@qq.com>
2021-01-19 15:48:18 +0800

    填充用户和话题数据

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 生成数据集合
+        User::factory()->count(10)->create();
+
+        // 单独处理第一个用户的数据
+        $user = User::find(1);
+        $user->name = 'Lemon';
+        $user->email = '918031078@qq.com';
+        $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        $user->save();
+    }
+}
