01ee0c413646d35a11a5ab6f2ee1cd6b7af18f60
huanggz <918031078@qq.com>
2021-01-19 21:15:48 +0800

    用户可以删除自己发的话题

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->hasMany(Topic::class);
     }
+
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
 }
