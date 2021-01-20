f8fbdbc9ba793650fc510be73710c30c95b973f6
huanggz <918031078@qq.com>
2021-01-20 12:37:38 +0800

    回复数据

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,4 +53,9 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->id == $model->user_id;
     }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
