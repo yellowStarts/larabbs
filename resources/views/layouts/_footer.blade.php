2a6957e0c677749aeaae28c47bfb0a6b6d5ab731
huanggz <918031078@qq.com>
2021-01-21 10:12:05 +0800

    站点设置

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
       由 <a href="https://weibo.com/xmlikepu/home?wvr=5" target="_blank">Lemon</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
     </p>
 
-    <p class="float-right"><a href="mailto:918031078@qq.com">联系我们</a></p>
+    <p class="float-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
   </div>
 </footer>
