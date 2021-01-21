c37e5b3a596d4697c3fa59179871fe9f57951be9
huanggz <918031078@qq.com>
2021-01-21 11:29:01 +0800

    活跃用户

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,6 +16,8 @@ class User extends Authenticatable implements MustVerifyEmailContract
 
     use HasRoles;
 
+    use Traits\ActiveUserHelper;
+
     use Notifiable {
         notify as protected laravelNotify;
     }
