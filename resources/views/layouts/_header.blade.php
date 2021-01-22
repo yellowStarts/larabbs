fd7d280e64d2a83f5b7d32ad6334341eadd92049
huanggz <918031078@qq.com>
2021-01-22 17:35:54 +0800

    logo

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -3,7 +3,7 @@
     <!-- Branding Image -->
     <a class="navbar-brand " href="{{ url('/') }}">
       {{-- {{ config('app.name') }} --}}
-      <img src="/favicon.ico" class="img-responsive" height="30px">
+      <img src="/logo.png" class="img-responsive" height="30px">
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
