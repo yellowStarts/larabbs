02a889a6911506fd3c447e13690c013da8811980
huanggz <918031078@qq.com>
2021-01-23 18:25:25 +0800

    频率限制

--- routes/api.php
+++ routes/api.php
@@ -14,12 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('v1')->namespace('Api\V1')->name('api.v1.')->group(function() {
-    // 短信验证码 验证接口
-    Route::post('verificationCodes', 'VerificationCodesController@store')->name('verificationCodes.store');
+Route::prefix('v1')
+    ->namespace('Api\V1')
+    ->name('api.v1.')
+    ->group(function() {
 
-    // 用户注册
-    Route::post('users', 'UsersController@store')->name('users.store');
+    Route::middleware('throttle:' . config('api.rate_limits.sign'))->group(function(){
+        // 短信验证码 验证接口
+        Route::post('verificationCodes', 'VerificationCodesController@store')->name('verificationCodes.store');
 
+        // 用户注册
+        Route::post('users', 'UsersController@store')->name('users.store');
+    });
+
+    Route::middleware('throttle:' . config('api.rate_limits.access'))->group(function(){
+
+    });
 
 });
