0322794eb708f0d324bf3d0c9a6a8a295914d7fc
huanggz <918031078@qq.com>
2021-01-23 16:06:24 +0800

    添加web路由注释

--- routes/web.php
+++ routes/web.php
@@ -14,22 +14,32 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
+// 首页
 Route::get('/', 'TopicsController@index')->name('root');
 
+// 用户认证
 Auth::routes(['verify' => true]);
 
+// 用户
 Route::resource('users', 'UsersController', ['show', 'update', 'edit']);
 
+// 话题
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
 
+// 话题 slug 专用路由
 Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
+// 分类
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
+// 上传图片
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 
+// 评论/回复
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
 
+// 通知
 Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
 
+// 后台鉴权
 Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
