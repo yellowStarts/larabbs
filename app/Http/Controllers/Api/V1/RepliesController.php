713141b268c6c9320ac53350a1393da2944285ba
huanggz <918031078@qq.com>
2021-01-25 14:19:11 +0800

    话题回复

---
+++ app/Http/Controllers/Api/V1/RepliesController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Requests\Api\V1\ReplyRequest;
+use App\Http\Resources\Api\V1\ReplyResource;
+use App\Models\Reply;
+use App\Models\Topic;
+use Illuminate\Http\Request;
+
+class RepliesController extends Controller
+{
+    public function store(ReplyRequest $request, Topic $topic, Reply $reply)
+    {
+        $reply->content = $request->content;
+        $reply->topic()->associate($topic);
+        $reply->user()->associate($request->user());
+        $reply->save();
+
+        return new ReplyResource($reply);
+    }
+}
--- app/Http/Controllers/Api/V1/TopicsController.php
+++ app/Http/Controllers/Api/V1/TopicsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Http\Queries\TopicQuery;
 use App\Http\Requests\Api\V1\TopicRequest;
 use App\Http\Resources\Api\V1\TopicResource;
 use App\Models\Topic;
@@ -12,32 +13,16 @@ use Spatie\QueryBuilder\AllowedFilter;
 
 class TopicsController extends Controller
 {
-    public function index(Request $request, Topic $topic)
+    public function index(Request $request, TopicQuery $query)
     {
-        $topics = QueryBuilder::for(Topic::class)
-                    ->allowedIncludes('user', 'category')
-                    ->allowedFilters([
-                        'title',
-                        AllowedFilter::exact('category_id'),
-                        AllowedFilter::scope('withOrder')->default('recentReplied'),
-                    ])
-                    ->paginate();
+        $topics = $query->paginate();
 
         return TopicResource::collection($topics);
     }
 
-    public function userIndex(Request $request, User $user)
+    public function userIndex(Request $request, User $user, TopicQuery $query)
     {
-        $query = $user->topics()->getQuery();
-
-        $topics = QueryBuilder::for($query)
-                    ->allowedIncludes('user', 'category')
-                    ->allowedFilters([
-                        'title',
-                        AllowedFilter::exact('category_id'),
-                        AllowedFilter::scope('withOrder')->default('recentReplied'),
-                    ])
-                    ->paginate();
+        $topics = $query->where('user_id', $user->id)->paginate();
 
         return TopicResource::collection($topics);
     }
@@ -47,6 +32,13 @@ class TopicsController extends Controller
         $topic->fill($request->all());
         $topic->user_id = $request->user()->id;
         $topic->save();
+
+        return new TopicResource($topic);
+    }
+
+    public function show($topicId, TopicQuery $query)
+    {
+        $topic = $query->findOrFail($topicId);
 
         return new TopicResource($topic);
     }
